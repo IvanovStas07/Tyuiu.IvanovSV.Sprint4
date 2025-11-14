3ca205667d764490c9e5519f5a3ebc04a1094d1c
IvanovStas07 <Tyuiu.IvanovSV@outlook.com>
2025-11-15 02:01:35 +0500

    Task3

---
+++ Tyuiu.IvanovSV.Sprint4.Task3.V3.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.IvanovSV.Sprint4.Task3.V3.Lib
+{
+    public class DataService : ISprint4Task3V5
+    {
+        public int Calculate(int[,] array)
+        {
+
+            int rows = array.GetUpperBound(0) + 1;    //количество строк
+            int columns = array.Length / rows;         //количество столбцов
+
+            int count = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (array[i, j] % 2 != 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+
+        }
+    }
+}
