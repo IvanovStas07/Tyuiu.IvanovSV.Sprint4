47a527988c3331bcc9a0830d8316eae17c4e742e
IvanovStas07 <Tyuiu.IvanovSV@outlook.com>
2025-11-15 01:12:19 +0500

    Task0

---
+++ Tyuiu.IvanovSV.Sprint4.Task0.V12.Lib/DataService.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.IvanovSV.Sprint4.Task0.V12.Lib
+{
+    public class DataService
+    {
+
+    }
+}
---
+++ Tyuiu.IvanovSV.Sprint4.Task0.V12.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.IvanovSV.Sprint4.Task0.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
