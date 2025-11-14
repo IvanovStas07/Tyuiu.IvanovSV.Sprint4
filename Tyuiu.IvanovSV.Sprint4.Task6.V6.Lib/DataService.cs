b054c4a09336747565f04f6eeaffe3710376037d
IvanovStas07 <Tyuiu.IvanovSV@outlook.com>
2025-11-15 02:42:25 +0500

    Task6

---
+++ Tyuiu.IvanovSV.Sprint4.Task6.V6.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.IvanovSV.Sprint4.Task6.V6.Lib
+{
+    public class DataService : ISprint4Task6V6
+    {
+        public string[] Calculate(string[] array)
+        {
+            string[] mas = Array.FindAll(array, x => x.Length == 5);
+            return mas;
+        }
+    }
+}
