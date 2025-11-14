a501ada5d71cb7309148e909b33559e9b17765c3
IvanovStas07 <Tyuiu.IvanovSV@outlook.com>
2025-11-15 01:21:18 +0500

    Task0

--- Tyuiu.IvanovSV.Sprint4.Task0.V12.Test/DataServiceTest.cs
+++ Tyuiu.IvanovSV.Sprint4.Task0.V12.Test/DataServiceTest.cs
@@ -1,11 +1,18 @@
-﻿namespace Tyuiu.IvanovSV.Sprint4.Task0.V12.Test
+﻿using Tyuiu.IvanovSV.Sprint4.Task0.V12.Lib;
+
+namespace Tyuiu.IvanovSV.Sprint4.Task0.V12.Test
 {
     [TestClass]
-    public sealed class DataServiceTest
+    public class DataServiceTest
     {
         [TestMethod]
         public void TestMethod1()
         {
+            DataService ds = new DataService();
+            int[] array = { 4, 6, 2, 8, 4, 5, 6, 9, 8, 7 };
+            int res = ds.GetMultEvenArrEl(array);
+            int wait = 73728;
+            Assert.AreEqual(res, wait);
         }
     }
 }
--- Tyuiu.IvanovSV.Sprint4.Task0.V12/Program.cs
+++ Tyuiu.IvanovSV.Sprint4.Task0.V12/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tyuiu.IvanovSV.Sprint4.Task0.V12.Lib;
+namespace Tyuiu.IvanovSV.Sprint4.Task0.V12
+{
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        int res;
+        DataService ds = new DataService();
+        Console.Title = "Спринт #4 | Выполнил Иванов С. В. | ИСТНб-25-1";
+        Console.WriteLine("*******************************************************************************");
+        Console.WriteLine("* Спринт #4                                                                   *");
+        Console.WriteLine("* Тема: Базовые навыки работы в с#                                            *");
+        Console.WriteLine("* Задание #0                                                                  *");
+        Console.WriteLine("* Вариант #12                                                                 *");
+        Console.WriteLine("* Выполнил Иванов С В | ИСТНю-25-1                           *");
+        Console.WriteLine("*******************************************************************************");
+        Console.WriteLine("* Условие                                                                     *");
+        Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный             *");
+        Console.WriteLine("* статическими значениями в диапазоне от 0 до 9 подсчитать произведение       *");
+        Console.WriteLine("* четных элементов массива.                                                   *");
+        Console.WriteLine("*******************************************************************************");
+        Console.WriteLine("* Исходные данные:                                                            *");
+        Console.WriteLine("*******************************************************************************");
+        int[] array = { 4, 6, 2, 8, 4, 5, 6, 9, 8, 7 };
+        Console.WriteLine("* Результат:                                                                  *");
+        Console.WriteLine("*******************************************************************************");
+        res = ds.GetMultEvenArrEl(array);
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
+}
